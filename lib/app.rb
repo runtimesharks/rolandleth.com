d21456a378fdfbd721bd7a719e589bf750b1e96a
Roland <rolandleth@me.com>
2013-06-30 21:57:30 +0300

    Files broke, working locally. Testing a push #2

--- lib/app.rb
+++ lib/app.rb
@@ -68,8 +68,7 @@ class Application < Sinatra::Application
 
 	# Apply a permanently redirected to http://root/name/ pointing at http://root/name
 	get %r{/([\w\s\.\}\{\]\[:"';!=\?\+\*\-\)\(]+)/} do |key|
-		puts key
-		redirect "/#{key}", 301
+		redirect "/#{key}", 301 unless key == 'files'
 	end
 
 	# Main page
