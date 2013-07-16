0f184b9557a8fd1141af8da02955dea3606a3d86
Roland <rolandleth@me.com>
2013-07-16 13:20:13 +0300

    Test #3

--- lib/app.rb
+++ lib/app.rb
@@ -114,6 +114,9 @@ class Application < Sinatra::Application
 
 		# Retrieve all posts in dir and store them in an array. sort the array, reverse it to be newest->oldest
 		all_posts = Dir['posts/*.md'].sort_by!{ |m| m.downcase }.reverse
+		all_posts.each do |post|
+			matches = post.match(/\/(\d{4})-(\d{2})-(\d{2})-([\w\s\.\}\{\[\]_&@$:"';!=\?\+\*\-\)\(]+)\.md$/)
+		end
 		page = (current_page || 1).to_i
 		# Start index is the first index on each page. if page == 2 and PAGE_SIZE == 5, start_index is 5
 		start_index = (page - 1) * PAGE_SIZE
