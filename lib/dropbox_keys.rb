7daf3fd7ae019c67cde2c98e537e165fbd6501e2
rolandleth <roland@leth.ro>
2015-05-05 00:38:34 +0300

    Added dropbox_keys.rb back into the repo

---
+++ lib/dropbox_keys.rb
@@ -0,0 +1,8 @@
+module DropboxKeys
+	APP_KEY = 'c2y0qfcv4l8da4u'
+	APP_SECRET = '006o5dpxcq1ph5x'
+	ACCESS_TYPE = :dropbox
+	AUTH_SECRET = '5agn40ihuth7gyt'
+	AUTH_KEY = 'vu3pnqmedjz789sg'
+  MY_SYNC_KEY = 'rolly'
+end
