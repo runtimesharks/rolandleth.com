273c86217d92e7f35e2e72aeee38754acaddb53e
Roland Leth <roland@leth.ro>
2015-05-29 10:52:30 +0300

    Improved image wrapping.

--- assets/javascripts/base.js
+++ assets/javascripts/base.js
@@ -1,3 +1,12 @@
-$(document).ready(function(){
+$(document).ready(function() {
+  //$('img:not([class])').wrap("<div class='centered-image-wrapper'></div>");
 
+  //setTimeout(function() {
+    $('img').each(function() {
+      //$(this).css('visibility', 'visible');
+      if ($(this).width() > $('section').width() && !$(this).hasClass()) {
+        $(this).wrap("<div class='centered-image-wrapper'></div>");
+      }
+    });
+  //}, 500);
 });
