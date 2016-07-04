5e1b56d30cab37478038353efd2eea1cb99bc1b3
Roland Leth <roland@leth.ro>
2016-07-04 14:33:27 +0300

    Layouts, 404

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 _ = require('lodash')
 Promise = require('bluebird')
 var app = require('express')()
+var engine = require('ejs-mate')
 var Mincer = require('mincer')
 
 var pipeline = new Mincer.Environment(__dirname)
@@ -14,6 +15,7 @@ pipeline.appendPath('assets/stylesheets')
 
 app.use('/assets/', Mincer.createServer(pipeline))
 app.use('/', require('./routes/routes'))
+app.engine('ejs', engine)
 app.set('view engine', 'ejs')
 
 app.listen(process.env.PORT || 3000)
--- routes/routes.js
+++ routes/routes.js
@@ -9,5 +9,11 @@ router.use('/about', require('./about'))
 router.use('/archive', require('./archive'))
 router.use('/privacy-policy', require('./privacy-policy'))
 router.use('/projects', require('./projects/projects'))
+router.use('*', function(req, res) {
+	res.render('not-found', {
+		title: 'Not found',
+		metadata: 'Development thoughts by Roland Leth.'
+	})
+})
 
 module.exports = router
