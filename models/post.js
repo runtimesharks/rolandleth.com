e67a23747c80e88d19d469714ec8f54b70c9b261
Roland Leth <roland@leth.ro>
2016-07-05 22:30:00 +0300

    Filtering of posts in the future

--- models/post.js
+++ models/post.js
@@ -3,20 +3,24 @@
  */
 
 function Post(title, body, readingTime, datetime, modified, link) {
-	var matches = datetime.match(/(\d{4})-(\d{2})-(\d{2})-(\d{4})/)
-	var year = matches[1]
-	var month = matches[2] - 1 // Months are 0 indexed :|
-	var day = matches[3]
-	var hour = matches[4].slice(0, 2)
-	var minute = matches[4].slice(2, 4)
-
 	this.title = title
 	this.body = body
 	this.readingTime = readingTime
 	this.datetime = datetime // For archive
-	this.date = new Date(year, month, day, hour, minute)
+	this.date = Post.dateFromDateTime(datetime)
 	this.modified = modified
 	this.link = link
+}
+
+Post.dateFromDateTime = function(datetime) {
+	var matches = datetime.match(/(\d{4})-(\d{2})-(\d{2})-(\d{4})/)
+	var year    = matches[1]
+	var month   = matches[2] - 1 // Months are 0 indexed :|
+	var day     = matches[3]
+	var hour    = matches[4].slice(0, 2)
+	var minute  = matches[4].slice(2, 4)
+
+	return new Date(year, month, day, hour, minute)
 }
 
 module.exports = Post
