e6470e4c14b5a92be8e648e7ddab13e1601248a8
Roland Leth <roland@Rolands-MacBook.local>
2016-10-24 23:03:41 +0300

    Updated resume, added Europass version

--- routes/downloads.js
+++ routes/downloads.js
@@ -12,15 +12,25 @@ router.get("/privacy-policy.md", function(req, res) {
 })
 
 router.get("/resume.pdf", function(req, res) {
-	const resume = "./assets/files/Resume.pdf"
+	const file = "./assets/files/Resume.pdf"
 	const fileName = "Roland Leth - Résumé.pdf"
-	fs.readFile(resume, function(err, data) {
+	sendPDF(file, fileName, res)
+})
+
+router.get("/europass.pdf", function(req, res) {
+	const file = "./assets/files/Europass.pdf"
+	const fileName = "Roland Leth - Europass.pdf"
+	sendPDF(file, fileName, res)
+})
+
+function sendPDF(file, fileName, res) {
+	fs.readFile(file, function(err, data) {
 		res.set({
 			"Content-Disposition": "inline; filename=" + fileName,
 			"Content-Type": "application/pdf; filename=" + fileName
 		})
 		res.send(data)
 	})
-})
+}
 
 module.exports = router
