115700d79295fe01e515438b48d842709bcf70d7
Roland Leth <roland@leth.ro>
2016-11-15 20:38:20 +0200

    Fixed tracking

--- assets/javascripts/base.js
+++ assets/javascripts/base.js
@@ -1,56 +1,52 @@
 $(document).ready(function() {
-	$(window).resize(function() {
-		resizeImage($('img:not([class])'));
-	});
+	
+	$(window).load(resizeImages)
+	$(window).resize(resizeImages)
+	
+	setupSearchField()
+})
 
-	setupSearchField();
-
-	var images = $('img:not([class])');
+function resizeImages() {
+	var images = $('img:not([class])')
 	for (var i = 0; i < images.length; i++) {
-		$(images[i]).on('load', function () {
-			resizeImage($(this));
-		});
+		resizeImage($(images[i]))
 	}
+}
 
-	//setTimeout(function() {
-	//	$('img').each(function() {
-	//		$(this).css('visibility', 'visible');
-	//	});
-	//}, 100);
-});
-
-var resizeImage = function(img) {
+function resizeImage(img) {
 	if (img.width() > $('body').width()) {
 		if (!img.closest('.centered-image-wrapper').length) {
-			img.wrap("<div class='centered-image-wrapper'></div>");
+			img.wrap("<div class='centered-image-wrapper'></div>")
 		}
 	}
 	else if (img.closest('.centered-image-wrapper').length) {
-		img.unwrap();
+		img.unwrap()
 	}
-};
+}
 
-var setupSearchField = function() {
-	var searchField = $('input.text-field');
+function setupSearchField() {
+	var searchField = $('input.text-field')
 
 	if (location.search.length) {
 		var query = decodeURIComponent(location.search)
 			.split("query=")[1]
-			.replace(/[\+]/g, " ");
+			.replace(/[\+]/g, " ")
 
-		searchField.val(query);
+		searchField.val(query)
 	}
 
 	// Smaller search fields for iPhones, since it doesn't fit at > 13
-	var winWidth = $(window).width();
-	var size = 17;
+	var winWidth = $(window).width()
+	var size = 17
+	
 	switch (true) {
-		case winWidth < 280: size = 10; break;
-		case winWidth <= 300: size = 13; break;
-		case winWidth < 310: size = 16; break;
-		case winWidth < 325: size = 18; break;
-		case winWidth < 335: size = 19; break;
-		default: size = 20; break;
+		case winWidth < 280: size = 10; break
+		case winWidth <= 300: size = 13; break
+		case winWidth < 310: size = 16; break
+		case winWidth < 325: size = 18; break
+		case winWidth < 335: size = 19; break
+		default: size = 20; break
 	}
-	searchField.attr('size', size);
-};
+	
+	searchField.attr('size', size)
+}
