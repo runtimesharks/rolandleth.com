2f0be3e0444ee84572d176efe6006a074f280b82
Roland Leth <roland@leth.ro>
2017-02-15 22:17:27 +0200

    Converted to classes, added license

--- models/dbConfig.js
+++ models/dbConfig.js
@@ -16,104 +16,106 @@
  * @property {int} limit - The number of posts to return, env.PAGE_SIZE || 10 by default.
  * @constructor
  */
-function DbConfig() {
-	this.updating       = false
-	this.searching      = false
-	this.columns        = "*"
-	this.fields         = null
-	this.fieldValues    = null
-	this.orderBy        = "datetime"
-	this.orderDirection = "ASC"
-	this.limit          = parseInt(process.env.PAGE_SIZE) || 10
-	this.offset         = 0
+class DbConfig {
+	
+	constructor() {
+		this.updating       = false
+		this.searching      = false
+		this.columns        = "*"
+		this.fields         = null
+		this.fieldValues    = null
+		this.orderBy        = "datetime"
+		this.orderDirection = "ASC"
+		this.limit          = parseInt(process.env.PAGE_SIZE) || 10
+		this.offset         = 0
+	}
+	
+	/** * Limit one, link as field, and the post"s link as fieldValue.
+	 * @param {String} link - The post"s link to fetch it by.
+	 * @returns {DbConfig} The {@link DbConfig} object.
+	 */
+	static post(link) {
+		const config       = new DbConfig()
+		config.fields      = ["link"]
+		config.fieldValues = [link]
+		config.limit       = 1
+		
+		return config
+	}
+	
+	/**
+	 * Offset set to limit * page - 1.
+	 * @param {int} page - The page we want posts for.
+	 * @returns {DbConfig} The {@link DbConfig} object.
+	 */
+	static page(page) {
+		const config  = new DbConfig()
+		config.offset = config.limit * (page - 1)
+		
+		return config
+	}
+	
+	/**
+	 * Zero limit and updating set to true.
+	 * @returns {DbConfig} The {@link DbConfig} object.
+	 */
+	static update() {
+		const config    = new DbConfig()
+		config.limit    = 0
+		config.updating = true
+		
+		return config
+	}
+	
+	/**
+	 * Zero limit.
+	 * @returns {DbConfig} The {@link DbConfig} object.
+	 */
+	static feed() {
+		const config = new DbConfig()
+		config.limit = 0
+		
+		return config
+	}
+	
+	/**
+	 * Zero limit, and link, modified and datetime columns.
+	 * @returns {DbConfig} The {@link DbConfig} object.
+	 */
+	static siteMap() {
+		const config   = new DbConfig()
+		config.limit   = 0
+		config.columns = "link, modified, datetime"
+		
+		return config
+	}
+	
+	/**
+	 * Zero limit, and title, link and datetime columns.
+	 * @returns {DbConfig} The {@link DbConfig} object.
+	 */
+	static archive() {
+		const config   = new DbConfig()
+		config.limit   = 0
+		config.columns = "title, link, datetime"
+		
+		return config
+	}
+	
+	/**
+	 * Zero limit, searching true, body and title as fields and the query as a field value.
+	 * @param {String} query - The strings we want to search for.
+	 * @returns {DbConfig} The {@link DbConfig} object.
+	 */
+	static search(query) {
+		const config       = new DbConfig()
+		config.limit       = 0
+		config.searching   = true
+		config.fields      = ["body", "title"]
+		config.fieldValues = [query]
+		
+		return config
+	}
+	
 }
-
-/**
- * Limit one, link as field, and the post"s link as fieldValue.
- * @param {String} link - The post"s link to fetch it by.
- * @returns {DbConfig} The {@link DbConfig} object.
- */
-DbConfig.post = function(link) {
-	const config       = new DbConfig()
-	config.fields      = ["link"]
-	config.fieldValues = [link]
-	config.limit       = 1
-
-	return config
-}
-
-/**
- * Offset set to limit * page - 1.
- * @param {int} page - The page we want posts for.
- * @returns {DbConfig} The {@link DbConfig} object.
- */
-DbConfig.page = function(page) {
-	const  config = new DbConfig()
-	config.offset = config.limit * (page - 1)
-
-	return config
-}
-
-/**
- * Zero limit and updating set to true.
- * @returns {DbConfig} The {@link DbConfig} object.
- */
-DbConfig.update = function() {
-	const config = new DbConfig()
-	config.limit = 0
-	config.updating = true
-
-	return config
-}
-
-/**
- * Zero limit.
- * @returns {DbConfig} The {@link DbConfig} object.
- */
-DbConfig.feed = function() {
-	const config = new DbConfig()
-	config.limit = 0
-
-	return config
-}
-
-/**
- * Zero limit, and link, modified and datetime columns.
- * @returns {DbConfig} The {@link DbConfig} object.
- */
-DbConfig.siteMap = function() {
-	const config = new DbConfig()
-	config.limit = 0
-	config.columns = "link, modified, datetime"
-
-	return config
-}
-
-/**
- * Zero limit, and title, link and datetime columns.
- * @returns {DbConfig} The {@link DbConfig} object.
- */
-DbConfig.archive = function() {
-	const  config  = new DbConfig()
-	config.limit   = 0
-	config.columns = "title, link, datetime"
-
-	return config
-}
-
-/**
- * Zero limit, searching true, body and title as fields and the query as a field value.
- * @param {String} query - The strings we want to search for.
- * @returns {DbConfig} The {@link DbConfig} object.
- */
-DbConfig.search = function(query) {
-	const config = new DbConfig()
-	config.limit = 0
-	config.searching = true
-	config.fields      = ["body", "title"]
-	config.fieldValues = [query]
-
-	return config
-}
-
 module.exports = DbConfig
--- models/post.js
+++ models/post.js
@@ -8,28 +8,6 @@ const readingTime = require("reading-time")
 const truncateHTML = require("truncate-html") // This one is really fast (400ms vs 80)
 require("../lib/date")
 
-/**
- * The Post model.
- * @param {String} title
- * @param {String} body
- * @param {String} datetime
- * @param {String} modified
- * @param {String} link
- * @param {String} readingTime
- * @param {String} truncatedBody
- * @constructor
- */
-function Post(title = "", body = "", datetime = "", modified = (new Date()).toDateString(),
-              link = Post.createLink(title), readingTime = timeToRead(body), truncatedBody = truncateBody(title, body, link)) {
-	this.title = title
-	this.body = body
-	this.truncatedBody = truncatedBody
-	this.readingTime = readingTime
-	this.datetime = datetime
-	this.date = Post.dateFromDateTime(datetime) || ""
-	this.modified = modified
-	this.link = link
-}
 
 /**
  * Creates a friedly reading time text.
@@ -69,78 +47,105 @@ function truncateBody(title, body, link) {
 }
 
 /**
- * Converts a string of yyyy-MM-dd format into a Date.
- * @param {String} datetime - yyyy-MM-dd format parameter
- * @returns {String|Date} A new Date corresponding to the parameter passed in, or an empty string.
+ * The Post model.
+ * @param {String} title
+ * @param {String} body
+ * @param {String} datetime
+ * @param {String} modified
+ * @param {String} link
+ * @param {String} readingTime
+ * @param {String} truncatedBody
+ * @constructor
  */
-Post.dateFromDateTime = function(datetime) {
-	if (!datetime) { return "" }
-	const matches = datetime.match(/(\d{4})-(\d{2})-(\d{2})-(\d{4})/)
-	const year    = matches[1]
-	const month   = matches[2] - 1 // Months are 0 indexed :|
-	const day     = matches[3]
-	const hour    = matches[4].slice(0, 2)
-	const minute  = matches[4].slice(2, 4)
-	const postDate = new Date(year, month, day, hour, minute)
+class Post {
 	
-	// Convert to UTC, but offset hours to match Bucharest timezone -- Not working, obviously :|
-	return new Date(Date.UTC(year, month, day, hour - (postDate.dst() ? 3 : 2), minute))
-}
-
-/**
- * Converts a date string into a date, then into a string of yyyy-MM-dd format.
- * @param {String} dateString - The string to be converted.
- * @returns {String} A string of yyyy-MM-dd format.
- */
-Post.datetimeFromDate = function(dateString) {
-	const date = new Date(dateString)
-	let month = (date.getMonth() + 1).toString()
-	let day = date.getDate().toString()
-	let hours = date.getHours().toString()
-	let minutes = date.getMinutes().toString()
-
-	if (month.length === 1) {
-		month = "0" + month
+	constructor(title = "", body = "", datetime = "", modified = (new Date()).toDateString(),
+              link = Post.createLink(title), readingTime = timeToRead(body), truncatedBody = truncateBody(title, body, link)) {
+		this.title         = title
+		this.body          = body
+		this.truncatedBody = truncatedBody
+		this.readingTime   = readingTime
+		this.datetime      = datetime
+		this.date          = Post.dateFromDateTime(datetime) || ""
+		this.modified      = modified
+		this.link          = link
 	}
-
-	if (day.length === 1) {
-		day = "0" + day
+	
+	/**
+	 * Converts a string of yyyy-MM-dd format into a Date.
+	 * @param {String} datetime - yyyy-MM-dd format parameter
+	 * @returns {String|Date} A new Date corresponding to the parameter passed in, or an empty string.
+	 */
+	static dateFromDateTime(datetime) {
+		if (!datetime) { return "" }
+		const matches = datetime.match(/(\d{4})-(\d{2})-(\d{2})-(\d{4})/)
+		const year    = matches[1]
+		const month   = matches[2] - 1 // Months are 0 indexed :|
+		const day     = matches[3]
+		const hour    = matches[4].slice(0, 2)
+		const minute  = matches[4].slice(2, 4)
+		const postDate = new Date(year, month, day, hour, minute)
+		
+		// Convert to UTC, but offset hours to match Bucharest timezone -- Not working, obviously :|
+		return new Date(Date.UTC(year, month, day, hour - (postDate.dst() ? 3 : 2), minute))
 	}
-
-	if (hours.length === 1) {
-		hours = "0" + hours
+	
+	/**
+	 * Tests if two posts have matching links, by checking for --X variations on the first parameter.
+	 * @param {Post} newPost - The file post.
+	 * @param {Post} post - The database post.
+	 * @returns {Boolean} True, if the two links match, false if they do not.
+	 */
+	static linksMatch(newPost, post) {
+		// Yea, yea, I know. But I"ll never have 100+ duplicates unknowingly :)
+		return newPost.link === post.link ||
+		       (newPost.link + "--") === post.link.slice(0, -1) ||
+		       (newPost.link + "--") === post.link.slice(0, -2)
 	}
-
-	if (minutes.length === 1) {
-		minutes = "0" + minutes
+	
+	/**
+	 * Converts a date string into a date, then into a string of yyyy-MM-dd format.
+	 * @param {String} dateString - The string to be converted.
+	 * @returns {String} A string of yyyy-MM-dd format.
+	 */
+	static datetimeFromDate(dateString) {
+		const date = new Date(dateString)
+		let month = (date.getMonth() + 1).toString()
+		let day = date.getDate().toString()
+		let hours = date.getHours().toString()
+		let minutes = date.getMinutes().toString()
+		
+		if (month.length === 1) {
+			month = "0" + month
+		}
+		
+		if (day.length === 1) {
+			day = "0" + day
+		}
+		
+		if (hours.length === 1) {
+			hours = "0" + hours
+		}
+		
+		if (minutes.length === 1) {
+			minutes = "0" + minutes
+		}
+		
+		return date.getFullYear() + "-" + month + "-" + day + "-" + hours + minutes
 	}
-
-	return date.getFullYear() + "-" + month + "-" + day + "-" + hours + minutes
-}
-
-/**
- * Tests if two posts have matching links, by checking for --X variations on the first parameter.
- * @param {Post} newPost - The file post.
- * @param {Post} post - The database post.
- * @returns {Boolean} True, if the two links match, false if they do not.
- */
-Post.linksMatch = function(newPost, post) {
-	// Yea, yea, I know. But I"ll never have 100+ duplicates unknowingly :)
-	return newPost.link === post.link ||
-	       (newPost.link + "--") === post.link.slice(0, -1) ||
-	       (newPost.link + "--") === post.link.slice(0, -2)
-}
-
-/**
- * Creates a link out of a title.
- * @param title The post's title.
- * @returns {String} A safe link.
- */
-Post.createLink = function(title) {
-	return title.replace(/([#,;!:"\'\?\[\]\{\}\(\$\/)]+)/g, "")
-		.replace(/&/g, "and")
-		.replace(/\s|\./g, "-")
-		.toLowerCase()
+	
+	/**
+	 * Creates a link out of a title.
+	 * @param title The post's title.
+	 * @returns {String} A safe link.
+	 */
+	static createLink(title) {
+		return title.replace(/([#,;!:"\'\?\[\]\{\}\(\$\/)]+)/g, "")
+			.replace(/&/g, "and")
+			.replace(/\s|\./g, "-")
+			.toLowerCase()
+	}
+	
 }
 
 module.exports = Post
