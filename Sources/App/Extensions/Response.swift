22295852642a43873ad6bbf78dfd105de36c1d82
Roland Leth <roland@leth.ro>
2017-02-17 14:26:36 +0200

    Updated gitignore, moved and added files

---
+++ Sources/App/Extensions/Response.swift
@@ -0,0 +1,15 @@
+//
+//  Response.swift
+//  romobos
+//
+//  Created by Roland Leth on 17.02.2017.
+//
+//
+
+import Foundation
+
+enum ResponseType: String {
+	case json = "application/json"
+	case html = "text/html"
+	case octetStream = "application/octet-stream"
+}
---
+++ Sources/App/Models/Post.swift
@@ -0,0 +1,19 @@
+//
+//  Post.swift
+//  roland
+//
+//  Created by Roland Leth on 17.02.2017.
+//
+//
+
+import Foundation
+
+struct Post {
+	
+	let title: String
+	let body: String
+	let truncatedBody: String
+	let readingTime: String
+	let datetime: String
+	
+}
