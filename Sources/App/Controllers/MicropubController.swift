43b3678b93f43a3398a89922fb46b8fafc31623d
Roland Leth <roland@leth.ro>
2018-05-17 14:15:10 +0300

    Added Micro.blog ping on micropost creation

--- Sources/App/Controllers/MicropubController.swift
+++ Sources/App/Controllers/MicropubController.swift
@@ -3,6 +3,7 @@
 // 
 //
 
+import Foundation
 import Vapor
 
 struct MicropubController {
@@ -10,7 +11,15 @@ struct MicropubController {
 	static func micropub(with request: Request) throws -> ResponseRepresentable {
 		try Micropost(from: request).save()
 		
+		updateMicroBlog()
+		
 		return Response(status: .ok)
+	}
+	
+	private static func updateMicroBlog() {
+		let microFeed = "https://rolandleth.com/microfeed".urlQueryPercentEncoded
+		
+		_ = try? drop.client.post("https://micro.blog/ping?url=\(microFeed)")
 	}
 	
 }
