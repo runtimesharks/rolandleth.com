dbe91949d555beeace9f530dcd5f7e1a8d045914
Roland Leth <roland@leth.ro>
2018-11-05 22:41:14 +0200

    Removed jQuery

--- Public/js/base.js
+++ Public/js/base.js
@@ -1,52 +1,43 @@
-$(document).ready(function() {
-	$(window).on("load", resizeImages)
-	$(window).on("resize", resizeImages)
-
-	setupSearchField()
-})
-
-function resizeImages() {
-	var images = $("img:not([class])")
-	for (var i = 0; i < images.length; i++) {
-		resizeImage($(images[i]))
-	}
+function ready(callback) {
+	// in case the document is already rendered
+	if (document.readyState != "loading") callback()
+	// modern browsers
+	else if (document.addEventListener)
+		document.addEventListener("DOMContentLoaded", callback)
+	// IE <= 8
+	else
+		document.attachEvent("onreadystatechange", () => {
+			if (document.readyState == "complete") callback()
+		})
 }
 
-function resizeImage(img) {
-	if (img.width() > $("body").width()) {
-		if (!img.closest(".centered-image-wrapper").length) {
-			img.wrap("<div class='centered-image-wrapper'></div>")
-		}
-	} else if (img.closest(".centered-image-wrapper").length) {
-		img.unwrap()
-	}
-}
+ready(setupSearchField)
 
 function setupSearchField() {
-	var searchField = $("input.text-field")
+	var searchField = document.querySelectorAll("input[class=text-field]")[0]
 
 	if (location.search.length) {
 		var query = decodeURIComponent(location.search)
 			.split("query=")[1]
 			.replace(/[\+]/g, " ")
 
-		searchField.attr("placeholder", "")
-		searchField.val(query)
+		searchField.setAttribute("placeholder", "")
+		searchField.value = query
 	}
 
 	// Smaller search fields for iPhones, since it doesn't fit at > 13
-	var winWidth = $(window).width()
+	var winWidth = window.innerWidth
 	var size = 17
 
 	// prettier-ignore
 	switch (true) {
 		case winWidth < 280: size = 10; break
-		case winWidth <= 385: size = 14; break
+		case winWidth <= 385: size = 13; break
 		case winWidth < 400: size = 16; break
 		case winWidth < 415: size = 18; break
 		case winWidth < 425: size = 19; break
 		default: return 20
 	}
 
-	searchField.attr("size", size)
+	searchField.setAttribute("size", size)
 }
