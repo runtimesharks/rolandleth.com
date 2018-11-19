92cd6ba0a8a50e8cd695f834a3e616dd8bc9613b
Roland Leth <roland@leth.ro>
2018-11-19 13:20:11 +0200

    Sitemap

---
+++ src/server/lib/sitemap.js
@@ -0,0 +1,50 @@
+import Db from "./db"
+
+async function createSitemap(res) {
+	const noPriority = ["privacy-policy", "feed"]
+	const lowPriority = [
+		"tech/projects/bouncyb",
+		"tech/projects/sosmorse",
+		"tech/projects/iwordjuggle",
+		"tech/projects/carminder"
+	]
+	const highPriority = ["about"]
+	const urls = noPriority.concat(lowPriority).concat(highPriority)
+
+	const rootPath = "https://rolandleth.com/tech/blog/"
+	let xml =
+		'<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'
+
+	urls.forEach(function(url) {
+		const priority = highPriority.includes(url)
+			? 0.9
+			: lowPriority.includes(url)
+			? 0.3
+			: 0.1
+		xml += "<url>"
+		xml += "<loc>" + rootPath + url + "</loc>"
+		xml += "<changefreq>" + "yearly" + "</changefreq>"
+		xml += "<priority>" + priority + "</priority>"
+		xml += "</url>"
+	})
+
+	const data = await Db.fetchSiteMapPosts()
+
+	data.posts.forEach(function(post) {
+		const datetime = post.modified.slice(0, -5)
+
+		xml += "<url>"
+		xml += "<loc>" + rootPath + post.link + "</loc>"
+		xml += "<changefreq>" + "monthly" + "</changefreq>"
+		xml += "<priority>" + 0.5 + "</priority>"
+		xml += "<lastmod>" + datetime + "</lastmod>"
+		xml += "</url>"
+	})
+
+	xml += "</urlset>"
+
+	res.header("Content-Type", "text/xml")
+	res.send(xml)
+}
+
+export default createSitemap
--- src/server/routes/router.js
+++ src/server/routes/router.js
@@ -5,12 +5,17 @@ import { downloadsRouter } from "./downloads"
 import { lifeRouter } from "./life"
 import { techRouter } from "./tech"
 import createFeed from "../lib/feed"
+import createSitemap from "../lib/sitemap"
 
 const router = express.Router()
 
 router.get("/feed", async (req, res) => {
 	createFeed("tech", req, res)
 })
+router.get("/sitemap.xml", async (req, res) => {
+	createSitemap(res)
+})
+
 router.get("/life", lifeRouter)
 router.use("/tech", techRouter)
 router.use("/api", apiRouter)
