dcdaecd3c57362275f4b9f15b17839d7a51526a6
Roland Leth <roland@leth.ro>
2018-12-09 10:53:08 +0200

    application/js header to postsFetcher

--- src/server/routes/api/postsFetcher.js
+++ src/server/routes/api/postsFetcher.js
@@ -3,9 +3,12 @@ import DbConfig from "../../models/dbConfig"
 
 async function fetchPosts(section, req, res) {
 	const config = DbConfig.page(req.query.page || 1)
+	config.section = section
+
 	const result = await Db.fetchPosts(config)
 	const posts = result.posts
 
+	setJSHeaderTo(res)
 	res.send(posts)
 }
 
@@ -13,7 +16,12 @@ async function fetchPost(link, res) {
 	const result = await Db.fetchPost(link)
 	const [post] = result.posts
 
+	setJSHeaderTo(res)
 	res.send(post)
+}
+
+function setJSHeaderTo(res) {
+	res.setHeader("Content-Type", "application/javascript")
 }
 
 export { fetchPosts, fetchPost }
