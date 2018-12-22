555e054f7cb538670e0120718f9469d0f1cb4155
Roland Leth <roland@leth.ro>
2018-12-22 14:27:02 +0200

    Added FeedRoute
    Used instead of BlogRoute and SearchRoute

--- src/components/routes/CommonRoutes.js
+++ src/components/routes/CommonRoutes.js
@@ -1,31 +1,26 @@
 import React from "react"
 import { Switch, Route } from "react-router-dom"
-import BlogRoute from "../pages/blog/BlogRoute"
 import Archive from "../pages/archive/Archive"
-import SearchRoute from "../pages/search/SearchRoute"
 import NotFoundPage from "../pages/NotFound"
+import FeedRoute from "./FeedRoute"
 
 const CommonRoutes = (props) => {
-	// We redirect here with a path of either `/tech`, or `/life`.
+	// We match here with a path of either `/tech`, or `/life`.
 	const section = props.path.substring(1, props.path.length)
+	const feedRoutes = [`${props.path}/search`, `${props.path}/blog/:postLink?`]
 
 	return (
 		<Switch>
 			<Route
 				exact
-				path={`/${section}/search`}
-				// This requires props for the `query` param.
-				render={(p) => <SearchRoute {...p} {...props} section={section} />}
+				path={feedRoutes}
+				// This requires props because `Search` requires the `query` param.
+				render={(p) => <FeedRoute {...p} {...props} section={section} />}
 			/>
 			<Route
 				exact
-				path={`/${section}/archive`}
+				path={`${props.path}/archive`}
 				render={() => <Archive section={section} />}
-			/>
-			<Route
-				exact
-				path={`/${section}/blog/:postLink?`}
-				render={(p) => <BlogRoute {...p} section={section} />}
 			/>
 			<Route path="*" component={NotFoundPage} />
 		</Switch>
