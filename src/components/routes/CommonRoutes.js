8876485b02e0f2c649126602ee3b0a0e8b2f57d6
Roland Leth <roland@leth.ro>
2018-12-22 17:21:58 +0200

    CreatePost page added

--- src/components/routes/CommonRoutes.js
+++ src/components/routes/CommonRoutes.js
@@ -3,11 +3,15 @@ import { Switch, Route } from "react-router-dom"
 import Archive from "../pages/archive/Archive"
 import NotFoundPage from "../pages/NotFound"
 import FeedRoute from "./FeedRoute"
+import configFile from "../../../config"
+import CreatePost from "../pages/CreatePost"
 
 const CommonRoutes = (props) => {
 	// We match here with a path of either `/tech`, or `/life`.
 	const section = props.path.substring(1, props.path.length)
 	const feedRoutes = [`${props.path}/search`, `${props.path}/blog/:postLink?`]
+	const envKey = process.env.NODE_ENV === "production" ? "prod" : "dev"
+	const createPostKey = configFile.CREATE_POST_KEY[envKey]
 
 	return (
 		<Switch>
@@ -21,6 +25,11 @@ const CommonRoutes = (props) => {
 				exact
 				path={`${props.path}/archive`}
 				render={() => <Archive section={section} />}
+			/>
+			<Route
+				exact
+				path={`${props.path}/create-post/${createPostKey}`}
+				render={(p) => <CreatePost {...props} />}
 			/>
 			<Route path="*" component={NotFoundPage} />
 		</Switch>
---
+++ src/server/routes/api/postCreator.js
@@ -0,0 +1,28 @@
+import Db from "../../lib/db"
+import marked from "marked"
+import Post from "../../models/post"
+
+async function createPost(section, req, res) {
+	const post = new Post(
+		req.body.title,
+		marked(req.body.body),
+		req.body.body,
+		undefined,
+		undefined,
+		1,
+		req.body.date,
+		undefined,
+		undefined,
+		undefined,
+		undefined,
+		undefined,
+		true
+	)
+
+	await Db.createPost(post, section)
+
+	res.setHeader("Content-Type", "application/javascript")
+	res.send({ post })
+}
+
+export default createPost
